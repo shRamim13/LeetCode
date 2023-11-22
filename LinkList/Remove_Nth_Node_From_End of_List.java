db3adece172ea152a96dfd46acac85588ffb7820
shRamim13 <shramim13@gmail.com>
2023-11-22 22:38:54 +0600

    l4

---
+++ LinkList/Remove_Nth_Node_From_End of_List.java
@@ -0,0 +1,43 @@
+public class Remove_Nth_Node_From_End of_List {
+    
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode reverse(ListNode head){
+        ListNode cur =head,prev=null;
+        while(cur!=null){
+            ListNode temp=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=temp;
+        }
+        return prev;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode s1=reverse(head);
+        int k=n-1;
+        if(k==0){
+           s1=s1.next;
+           return reverse(s1);
+        }
+        ListNode ab=s1;
+        {
+            for(int i=0;i<k-1;i++){
+                ab=ab.next;
+            }
+            ab.next=ab.next.next;
+            return reverse(s1);
+        }
+    }
+}
