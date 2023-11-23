7a41a796e716689c7b9bec2871343983e6474bdf
shRamim13 <shramim13@gmail.com>
2023-11-23 19:25:48 +0600

    l5

---
+++ LinkList/AddTwoNumbers.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode cur = ans;
+
+        int carray = 0;
+
+        while (l1 != null || l2 != null) {
+            int sum = 0;
+            sum = sum + carray;
+            if (l1 != null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+            carray = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+        }
+        if (carray == 1) {
+            cur.next = new ListNode(carray);
+        }
+
+        return ans.next;
+    }
+}
