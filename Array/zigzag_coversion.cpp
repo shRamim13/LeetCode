79a070c3b55fb4128151134ef7e28e3009df1149
shRamim13 <shramim13@gmail.com>
2024-01-09 13:43:32 +0600

    1

---
+++ Array/zigzag_coversion.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    string convert(string s, int numRows)
+    {
+        vector<string> v(numRows);
+        if (numRows == 1)
+        {
+            return s;
+        }
+        bool flag = false;
+        string ans = "";
+        int i = 0;
+        for (auto ch : s)
+        {
+            v[i] += ch;
+            if (i == 0 || i == numRows - 1)
+            {
+                flag = !flag;
+            }
+            if (flag)
+            {
+                i++;
+            }
+            else
+            {
+                i--;
+            }
+        }
+        for (auto k : v)
+        {
+            ans += k;
+        }
+        return ans;
+    }
+};
