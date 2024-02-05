b6ea69048eba00ec8b6541e4ed313955d21e5f7c
shRamim13 <shramim13@gmail.com>
2024-02-05 21:17:06 +0600

    ok

--- Recursion/PrintAllPermutation.cpp
+++ Recursion/PrintAllPermutation.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void permutation(int indx, vector<int> nums, vector<int> v, vector<vector<int>> ans)
+void permutation(int indx, vector<int> freq, vector<int> nums, vector<int> v, vector<vector<int>> ans)
 {
+    if (v.size() == nums.size())
+    {
+        ans.push_back(v);
+        return;
+    }
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (!freq[i])
+        {
+            v.push_back(nums[i]);
+            freq[i] = 1;
+            permutation(i, freq, nums, v, ans);
+            v.pop_back();
+            freq[i] = 0;
+        }
+    }
 }
+
 int main()
 {
     int n;
@@ -11,12 +28,13 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        int x = 0;
-        cin >> nums.push_back(x);
+        int x ;
+         nums.push_back(x);
     }
     vector<int> v;
     vector<vector<int>> ans;
-    permutation(0, nums, v, ans);
+    vector<int> freq(n, 0);
+    permutation(0, freq, nums, v, ans);
     for (auto x : ans)
     {
         for (auto y : x)
