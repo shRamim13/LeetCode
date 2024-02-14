fc860cba6d368fd652cb4946ddf135e0f27f1e65
shRamim13 <shramim13@gmail.com>
2024-02-14 23:33:07 +0600

    singlenumber

---
+++ Array/MoveZeros.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[cnt++] = nums[i];
+            }
+        }
+        for (; cnt < nums.size(); cnt++)
+        {
+            nums[cnt] = 0;
+        }
+    }
+}; 
