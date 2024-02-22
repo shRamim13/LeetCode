362fc2275ec6ab5d8c77162aac528f03df99011f
shRamim13 <shramim13@gmail.com>
2024-02-22 19:06:18 +0600

    Moore votting algorithm

---
+++ Array/MajorityElements.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int mjrElmnt = 0, cnt = 0;
+        for (auto x : nums)
+        {
+            if (cnt == 0)
+            {
+                mjrElmnt = x;
+            }
+            if (mjrElmnt == x)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+        return mjrElmnt;
+    }
+};
--- Array/Majority_Element.cpp
+++ Array/Majority_Element.cpp
@@ -1,25 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-    int majorityElement(vector<int> &nums)
-    {
-        int n = nums.size();
-        if (n == 0) // handle empty vector
-            return -1;
-
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < n; i++)
-        {
-            mp[nums[i]]++;
+    int majorityElement(vector<int>& nums) {
+    int mjrElmnt=0,cnt=0;
+        for(auto x:nums){
+            if(cnt==0){
+                mjrElmnt=x;
+            }
+            if(mjrElmnt==x){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
-        n = n / 2;
-        for (auto x : mp)
-        {
-            if (x.second > n)
-                return x.first;
-        }
-
-        return -1;
+        return mjrElmnt;
     }
 };
