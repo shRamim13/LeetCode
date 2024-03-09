e50d0a1680e8e56194dffdf23437f769b8ccb27a
shRamim13 <shramim13@gmail.com>
2024-03-09 22:57:20 +0600

    1

--- String/SortCharactersByFrequency.cpp
+++ String/SortCharactersByFrequency.cpp
@@ -14,7 +14,37 @@ public:
         {
             s += string(maxheap.top().first, maxheap.top().second);
             maxheap.pop();
-    
-        return s;
-    }
-};
+
+            return s;
+        }
+    };
+
+    /*
+    class Solution {
+    public:
+        string frequencySort(string s) {
+            unordered_map<char, int> mp;
+            vector<pair<int, char>> v;
+            string ans = "";
+
+            // count character frequency
+            for(auto ch: s){
+                mp[ch]++;
+            }
+
+            // push from map to vector
+            for(auto i: mp){
+                v.push_back({i.second, i.first});
+            }
+
+            // sort the vector in decreasing order
+            sort(v.begin(), v.end(), greater<pair<int, char>>());
+
+            // add to final answer string
+            for(auto i: v){
+                while(i.first--) ans += i.second;
+            }
+            return ans;
+        }
+    };
+    */
