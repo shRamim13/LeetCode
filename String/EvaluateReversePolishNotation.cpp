c5c08b069eebef1182bda3b09a484a04c608e4e1
shRamim13 <shramim13@gmail.com>
2024-03-11 10:55:26 +0600

    st

---
+++ String/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,48 @@
+class Solution
+{
+public:
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<int> st;
+        for (auto x : tokens)
+        {
+            if (x == "+")
+            {
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                st.push(b + a);
+            }
+            else if (x == "-")
+            {
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                st.push(b - a);
+            }
+            else if (x == "*")
+            {
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                st.push(b * a);
+            }
+            else if (x == "/")
+            {
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                st.push(b / a);
+            }
+            else
+            {
+                st.push(stoi(x));
+            }
+        }
+        return st.top();
+    }
+};
