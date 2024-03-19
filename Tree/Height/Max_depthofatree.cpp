4253691ad1bf8c23987103f7ee0d61fb539a72d6
shRamim13 <shramim13@gmail.com>
2024-03-19 23:20:01 +0600

    s

---
+++ Tree/Height/Max_depthofatree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int treeHeight(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        int leftHeight = treeHeight(root->left);
+        int rightHeight = treeHeight(root->right);
+        return 1 + max(leftHeight, rightHeight);
+    }
+
+    int maxDepth(TreeNode *root)
+    {
+        return treeHeight(root);
+    }
+};
