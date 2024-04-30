34a6b8ce5eb99750901bdf805388f4ea2d2ef662
shRamim13 <shramim13@gmail.com>
2024-04-30 22:20:36 +0600

    1

---
+++ Greedy/NMeetingsinOneRoom.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxMeetings(int start[], int end[], int n)
+{
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back({end[i], start[i]});
+    }
+    sort(v.begin(), v.end(), [](const auto &a, const auto &b)
+         { return (a.first != b.first) ? a.first < b.first : a.second < b.second; });
+
+    int cnt = 0;
+    int endTask = 0;
+    for (const auto &x : v)
+    {
+        if (x.second >= endTask)
+        {
+            cnt++;
+            endTask = x.first;
+        }
+    }
+    return cnt;
+}
