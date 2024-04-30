356b746aeca61dcdc8475e8405d4fe45aaf01ad3
shRamim13 <shramim13@gmail.com>
2024-04-30 15:07:34 +0600

    s

---
+++ Greedy/LemonadeChange.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    bool lemonadeChange(vector<int> &bills)
+    {
+        int count5 = 0, count10 = 0, count20 = 0;
+
+        for (auto bill : bills)
+        {
+            if (bill == 5)
+            {
+                count5++;
+            }
+            else if (bill == 10)
+            {
+                if (count5 < 1)
+                {
+                    return false;
+                }
+                count5--;
+                count10++;
+            }
+            else
+            {
+                if (count10 > 0 && count5 > 0)
+                {
+                    count10--;
+                    count5--;
+                }
+                else if (count5 > 2)
+                {
+                    count5 -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+                count20++;
+            }
+        }
+        return true;
+    }
+};
