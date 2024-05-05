7643e37414772e6a844a85384c9b6d4c2604b5f5
shRamim13 <shramim13@gmail.com>
2024-05-05 22:25:43 +0600

    h

--- Heap/HeapPractice.cpp
+++ Heap/HeapPractice.cpp
@@ -33,12 +33,103 @@ public:
     int extractMin();
     void decreaseKey(int i, int new_val);
     void deleteKey(int i);
+    int getMax()
+    {
+        return arr[0];
+    }
     void insertKey(int i);
-}
+};
 
 MaxHeap::MaxHeap(int cap)
 {
     heap_size = 0;
     capacity = cap;
     arr = new int[cap];
+}
+
+void MaxHeap::insertKey(int k)
+{
+    if (heap_size == capacity)
+    {
+        cout << "\nOverFlow: Could not insertKey\n";
+        return;
+    }
+    heap_size++;
+    int i = heap_size - 1;
+    arr[i] = k;
+    while (i != 0 && arr[parent(i)] < arr[i])
+    {
+        swap(arr[parent(i)], arr[i]);
+        i = parent(i);
+    }
+}
+
+void MaxHeap::decreaseKey(int i, int new_val)
+{
+    arr[i] = new_val;
+    while (i != 0 && arr[parent(i)] < arr[i])
+    {
+        swap(arr[parent(i)], arr[i]);
+        i = parent(i);
+    }
+}
+
+int MaxHeap::extractMin()
+{
+    if (heap_size <= 0)
+    {
+        return INT_MIN;
+    }
+    if (heap_size == 1)
+    {
+        heap_size--;
+        return arr[0];
+    }
+    int root = arr[0];
+    arr[0] = arr[heap_size - 1];
+    heap_size--;
+    MaxHeapify(0);
+    return root;
+}
+void MaxHeap::deleteKey(int i)
+{
+    decreaseKey(i, );
+    cout << extractMin() << " is deleted from heap \n";
+}
+
+void MaxHeap::MaxHeapify(int i)
+{
+    int lt = leftChild(i);
+    int rt = RightChild(i);
+    int largest = i;
+    if (lt < heap_size && arr[lt] > arr[largest])
+    {
+        largest = lt;
+    }
+    if (rt < heap_size && arr[rt] > arr[largest])
+    {
+        largest = rt;
+    }
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        MaxHeapify(largest);
+    }
+}
+
+int main()
+{
+    MaxHeap h(11);
+    h.insertKey(3);
+    h.insertKey(2);
+    h.deleteKey(1);
+    h.insertKey(15);
+    h.insertKey(5);
+    h.insertKey(4);
+    h.insertKey(45);
+    cout << h.extractMin() << " ";
+    cout << h.getMax() << " ";
+    h.decreaseKey(2, 1);
+    cout << h.getMax();
+    return 0;
 }
