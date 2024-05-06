623c0ed1bcdce314bd50f21035dd5d2bb3e88af4
shRamim13 <shramim13@gmail.com>
2024-05-06 17:07:09 +0600

    h

--- Heap/KthLargestElementinanArray.cpp
+++ Heap/KthLargestElementinanArray.cpp
@@ -5,7 +5,9 @@ public:
     {
 
         auto cmp = [](int a, int b)
-        { return a > b; };
+        {
+            return a > b;
+        };
 
         priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
 
