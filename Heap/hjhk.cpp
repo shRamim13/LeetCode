2687b84754887d49427c9d4c7199eddcf41422ac
shRamim13 <shramim13@gmail.com>
2024-05-08 22:39:29 +0600

    h

---
+++ Heap/hjhk.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>> pq;
+        for (auto val : arr) {
+            pq.push({(abs(x - val)), val});
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> result;
+        while (!pq.empty()) {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
