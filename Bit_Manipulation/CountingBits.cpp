2526752bf1908efa207d0731a99efa25893eb068
shRamim13 <shramim13@gmail.com>
2024-05-11 00:03:24 +0600

    b

---
+++ Bit_Manipulation/CountingBits.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    vector<int> countBits(int n)
+    {
+        vector<int> v(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            int cnt = 0;
+            int num = i;
+            while (num > 0)
+            {
+                cnt++;
+                num = num & (num - 1);
+            }
+            v[i] = cnt;
+        }
+        return v;
+    }
+};
