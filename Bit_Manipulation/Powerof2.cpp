5421c2a1f31f4656cc343620e4697c3826a41439
shRamim13 <shramim13@gmail.com>
2024-05-10 22:46:29 +0600

    b

--- Bit_Manipulation/Powerof2.cpp
+++ Bit_Manipulation/Powerof2.cpp
@@ -3,10 +3,8 @@ class Solution
 public:
     bool isPowerOfTwo(int n)
     {
-        if (n == 0)
-        {
+        if (n <= 0)
             return 0;
-        }
         return (n & (n - 1)) ? 0 : 1;
     }
 };
