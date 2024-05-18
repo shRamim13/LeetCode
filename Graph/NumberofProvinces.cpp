cffb1de126a6d3aee7c0456bd02437ef831d1f59
shRamim13 <shramim13@gmail.com>
2024-05-18 15:13:01 +0600

    g

--- Graph/NumberofProvinces.cpp
+++ Graph/NumberofProvinces.cpp
@@ -1,32 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution
 {
 private:
-    void dfs(int node, vector<int> &vis, vector<vector<int>> &isConnected)
+    void dfs(int node, vector<int> &vis, vector<vector<int>> &adj)
     {
         vis[node] = 1;
-        for (int i = 0; i < isConnected[node].size(); i++)
+        for (int i = 0; i < adj[node].size(); i++)
         {
-            if (isConnected[node][i] == 1 && vis[i] == 0)
+            if (adj[node][i] == 1 && vis[i] == 0)
             {
-
-                dfs(i, vis, isConnected);
+                dfs(i, vis, adj);
             }
         }
     }
 
 public:
-    int findCircleNum(vector<vector<int>> &isConnected)
+    int numProvinces(vector<vector<int>> &adj, int V)
     {
-        vector<int> vis(isConnected.size(), 0);
+        vector<int> vis(V, 0);
         int cnt = 0;
-        for (int i = 0; i < isConnected.size(); i++)
+
+        for (int i = 0; i < V; i++)
         {
             if (vis[i] == 0)
             {
                 cnt++;
-                dfs(i, vis, isConnected);
+                dfs(i, vis, adj);
             }
         }
+
         return cnt;
     }
 };
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, x;
+        cin >> V;
+
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < V; i++)
+        {
+            vector<int> temp;
+            for (int j = 0; j < V; j++)
+            {
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution ob;
+        cout << ob.numProvinces(adj, V) << endl;
+    }
+    return 0;
+}
