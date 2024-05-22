6063d7751cc44b18152fa0759893de354d657e7e
shRamim13 <shramim13@gmail.com>
2024-05-22 15:05:51 +0600

    a

---
+++ Array/PalindromeNumber.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+        while (x != 0)
+        {
+            int digit = x % 10;
+            if (reversed > (INT_MAX - digit) / 10)
+            {
+                return false;
+            }
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reversed;
+    }
+};
