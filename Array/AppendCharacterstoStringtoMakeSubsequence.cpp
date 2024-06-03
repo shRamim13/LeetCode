aefdf9bdbbf102359bbd13c77e024c865fa2cae3
shRamim13 <shramim13@gmail.com>
2024-06-03 23:17:22 +0600

    arr

---
+++ Array/AppendCharacterstoStringtoMakeSubsequence.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int appendCharacters(string s, string t)
+    {
+        int it = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < t.size(); i++)
+        {
+            it = s.find(t[i], it);
+            if (it != string::npos)
+            {
+                it++;
+                cnt++;
+            }
+            else
+            {
+
+                break;
+            }
+        }
+
+        return t.size() - cnt;
+    }
+};
