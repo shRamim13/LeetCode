3c504ac792b2b879386cb1ea09de8c8d1677af3b
shRamim13 <shramim13@gmail.com>
2024-06-06 13:13:17 +0600

    s

--- Graph/Topological_Sort/ArticulationPoint.cpp
+++ Graph/Topological_Sort/ArticulationPoint.cpp
@@ -1,62 +1,97 @@
-private:
-int timer = 1;
-void dfs(int node, int parent, vector<int> &vis, int tin[], int low[],
-         vector<int> &mark, vector<int> adj[])
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
 {
-    vis[node] = 1;
-    tin[node] = low[node] = timer;
-    timer++;
-    int child = 0;
-    for (auto it : adj[node])
+private:
+    int timer = 1;
+    void dfs(int node, int parent, vector<int> &vis, int tin[], int low[],
+             vector<int> &mark, vector<int> adj[])
     {
-        if (it == parent)
-            continue;
-        if (!vis[it])
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        int child = 0;
+        for (auto it : adj[node])
         {
-            dfs(it, node, vis, tin, low, mark, adj);
-            low[node] = min(low[node], low[it]);
-            if (low[it] >= tin[node] && parent != -1)
+            if (it == parent)
+                continue;
+            if (!vis[it])
             {
-                mark[node] = 1;
+                dfs(it, node, vis, tin, low, mark, adj);
+                low[node] = min(low[node], low[it]);
+                if (low[it] >= tin[node] && parent != -1)
+                {
+                    mark[node] = 1;
+                }
+                child++;
             }
-            child++;
+            else
+            {
+                low[node] = min(low[node], tin[it]);
+            }
         }
-        else
+        if (child > 1 && parent == -1)
         {
-            low[node] = min(low[node], tin[it]);
+            mark[node] = 1;
         }
     }
-    if (child > 1 && parent == -1)
-    {
-        mark[node] = 1;
-    }
-}
 
 public:
-vector<int> articulationPoints(int n, vector<int> adj[])
+    vector<int> articulationPoints(int n, vector<int> adj[])
+    {
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<int> mark(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i, -1, vis, tin, low, mark, adj);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            if (mark[i] == 1)
+            {
+                ans.push_back(i);
+            }
+        }
+        if (ans.size() == 0)
+            return {-1};
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
 {
-    vector<int> vis(n, 0);
-    int tin[n];
-    int low[n];
-    vector<int> mark(n, 0);
-    for (int i = 0; i < n; i++)
+    int tc;
+    cin >> tc;
+    while (tc--)
     {
-        if (!vis[i])
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++)
         {
-            dfs(i, -1, vis, tin, low, mark, adj);
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
+        Solution obj;
+        vector<int> ans = obj.articulationPoints(V, adj);
+        for (auto i : ans)
+            cout << i << " ";
+        cout << "\n";
     }
-    vector<int> ans;
-    for (int i = 0; i < n; i++)
-    {
-        if (mark[i] == 1)
-        {
-            ans.push_back(i);
-        }
-    }
-    if (ans.size() == 0)
-        return {-1};
-    return ans;
+    return 0;
 }
-}
-;
