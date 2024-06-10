0a0b1379422ddb63f5594a9eee9e0100e57b535e
shRamim13 <shramim13@gmail.com>
2024-06-10 13:16:58 +0600

    s

--- SlidingWindow_2pointer_hashMap/2Sum.cpp
+++ SlidingWindow_2pointer_hashMap/2Sum.cpp
@@ -20,3 +20,21 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//      unordered_map<int,int>mp;
+//      for(int i=0;i<nums.size();i++){
+//          mp[nums[i]]=i;
+//      }
+//      for(int i=0;i<nums.size();i++){
+//          int x=target-nums[i];
+//          if(mp.count(x)&&i!=mp[x]){
+//              return {i,mp[x]};
+//         }
+//     }
+//     return {};
+
+//     }
+// };
