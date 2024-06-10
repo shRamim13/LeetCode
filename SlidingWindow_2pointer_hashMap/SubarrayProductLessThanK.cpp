daa7750f0deec1415e534d23d8029263039f5456
shRamim13 <shramim13@gmail.com>
2024-06-10 15:37:02 +0600

    s

--- SlidingWindow_2pointer_hashMap/SubarrayProductLessThanK.cpp
+++ SlidingWindow_2pointer_hashMap/SubarrayProductLessThanK.cpp
@@ -3,21 +3,27 @@ class Solution
 public:
     int numSubarrayProductLessThanK(vector<int> &nums, int k)
     {
+        if (k <= 1)
+            return 0;
+
         int cnt = 0;
         int mul = 1;
         int n = nums.size();
         int i = 0, j = 0;
+
         while (j < n)
         {
             mul *= nums[j];
-            if (i < n && mul >= k)
+            while (mul >= k)
             {
                 mul /= nums[i];
                 i++;
             }
-            cnt += (j - i + 1);
+            if (mul < k)
+                cnt += (j - i + 1);
             j++;
         }
+
         return cnt;
     }
 };
