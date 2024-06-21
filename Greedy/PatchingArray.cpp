24b174db4d5f14bedfce6ed18efb8e9050e4ee4a
shRamim13 <shramim13@gmail.com>
2024-06-21 12:22:03 +0600

    s

---
+++ Greedy/PatchingArray.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int minPatches(vector<int> &nums, int n)
+    {
+        long formedSum = 0;
+        int expectedSum = 1;
+        int minpatch = 0;
+        int i = 0;
+        int sz = nums.size();
+
+        while (formedSum < n)
+        {
+            if (i < sz && nums[i] <= expectedSum)
+            {
+                formedSum += nums[i];
+                i++;
+            }
+            else
+            {
+                minpatch++;
+                formedSum += expectedSum;
+            }
+            expectedSum = formedSum + 1;
+        }
+
+        return minpatch;
+    }
+};
