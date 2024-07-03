1ad2ad2f984ef07577647edf55ec9160e746bb0c
shRamim13 <shramim13@gmail.com>
2024-07-03 21:06:57 +0600

    s

--- Recursion/Generateallbinarystringswithoutconsecutive1s.cpp
+++ Recursion/Generateallbinarystringswithoutconsecutive1s.cpp
@@ -1,46 +1,28 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+void solve(int n, string temp, vector<string> &ans)
 {
-public:
-    void generateStrings(int n, string current, vector<string> &result)
+    if (n == temp.size())
     {
-        // Base case: if the current string length is n, add it to the result
-        if (current.length() == n)
-        {
-            result.push_back(current);
-            return;
-        }
-
-        // Add '0' and recursively call the function
-        generateStrings(n, current + "0", result);
-
-        // Add '1' and recursively call the function only if the last character is not '1'
-        if (current.empty() || current.back() != '1')
-        {
-            generateStrings(n, current + "1", result);
-        }
+        ans.push_back(temp);
+        return;
     }
-
-    vector<string> generateBinaryStrings(int n)
+    solve(n, temp + '0', ans);
+    if (temp.size() == 0 || temp.back() != '1')
     {
-        vector<string> result;
-        generateStrings(n, "", result);
-        return result;
+        solve(n, temp + '1', ans);
     }
-};
+}
 
 int main()
 {
-    Solution solution;
-    int n = 5; // Length of the binary strings
-    vector<string> binaryStrings = solution.generateBinaryStrings(n);
-
-    for (const string &str : binaryStrings)
+    int n;
+    cin >> n;
+    vector<string> ans;
+    solve(n, "", ans);
+    for (auto x : ans)
     {
-        cout << str << endl;
+        cout << x << endl;
     }
-
-    return 0;
 }
