77964bd0b5b156ea00c8d69c79a9c8f01ebcd6a8
shRamim13 <shramim13@gmail.com>
2024-07-04 14:51:51 +0600

    123456

---
+++ Recursion/GenerateParentheses.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    vector<string> ans;
+
+    void _genarate_parenthese(string &s, int first, int second)
+    {
+        if (first == 0 && second == 0)
+        {
+            ans.push_back(s);
+            return;
+        }
+
+        if (first > 0)
+        {
+            s.push_back('(');
+            _genarate_parenthese(s, first - 1, second);
+            s.pop_back();
+        }
+
+        if (second > 0)
+        {
+            if (second > first)
+            {
+                s.push_back(')');
+                _genarate_parenthese(s, first, second - 1);
+                s.pop_back();
+            }
+        }
+    }
+
+    vector<string> generateParenthesis(int n)
+    {
+        string s;
+        _genarate_parenthese(s, n, n);
+        return ans;
+    }
+};
