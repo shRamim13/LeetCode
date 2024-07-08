f58433af153759e47232cc40c1c0c9c1f2749552
shRamim13 <shramim13@gmail.com>
2024-07-08 23:13:09 +0600

    s

--- Recursion/CombinationSum2.cpp
+++ Recursion/CombinationSum2.cpp
@@ -1,27 +1,34 @@
-class Solution {
+class Solution
+{
 public:
-    void func(int index,int target,vector<int>&candidates,vector<int>&v,vector<vector<int>>&ans){
-        if(target==0){
+    void func(int index, int target, vector<int> &candidates, vector<int> &v, vector<vector<int>> &ans)
+    {
+        if (target == 0)
+        {
             ans.push_back(v);
-            return ;
+            return;
         }
-        for(int i=index;i<candidates.size();i++){
-            if(i>index&&candidates[i]==candidates[i-1])continue;
-            if(candidates[i]>target){
+        for (int i = index; i < candidates.size(); i++)
+        {
+            if (i > index && candidates[i] == candidates[i - 1])
+                continue;
+            if (candidates[i] > target)
+            {
                 break;
             }
-        v.push_back(candidates[i]);
-        func(i+1,target-candidates[i],candidates,v,ans);
-        v.pop_back();
-         } 
+            v.push_back(candidates[i]);
+            func(i + 1, target - candidates[i], candidates, v, ans);
+            v.pop_back();
+        }
     }
 
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        sort(candidates.begin(),candidates.end());
-        vector<int>v;
-        int index=0;
-        func(index,target,candidates,v,ans);
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+        vector<int> v;
+        int index = 0;
+        func(index, target, candidates, v, ans);
         return ans;
     }
 };
