7030fbea916e30792a7a4f3c5105e0d7612f319d
shramim13 <shramim13@gmail.com>
2024-08-23 12:57:00 +0600

    s

---
+++ Stack_QUEUE/Basic/ValidParentheses.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    return false;
+                }
+                else
+                {
+                    char ch = st.top();
+                    if (s[i] == ')' && ch == '(' || s[i] == '}' && ch == '{' ||
+                        s[i] == ']' && ch == '[')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return st.empty();
+    }
+};
