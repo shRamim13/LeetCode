c661511e8a217dec11973875ee1a7b5555443ea0
shramim13 <shramim13@gmail.com>
2024-09-08 23:26:59 +0600

    s

--- Array/H_Index.cpp
+++ Array/H_Index.cpp
@@ -1,12 +1,46 @@
-	class Solution {
-	public:
-		int hIndex(vector<int>& citations) {
-			int index;
-			sort(citations.begin(),citations.end(),greater<int>());
-			int n=citations.size();
-			for (index = 0;index<n;index++){
-				if(index>=citations[index])break;
+// class Solution {
+// public:
+// 	int hIndex(vector<int>& citations) {
+// 		int index;
+// 		sort(citations.begin(),citations.end(),greater<int>());
+// 		int n=citations.size();
+// 		for (index = 0;index<n;index++){
+// 			if(index>=citations[index])break;
+// 		}
+// 		return index;
+// 	}
+// };
+
+class Solution
+{
+public:
+	int hIndex(vector<int> &citations)
+	{
+		int n = citations.size();
+		vector<int> count(n + 1, 0);
+
+		for (int citation : citations)
+		{
+			if (citation >= n)
+			{
+				count[n]++;
 			}
-			return index;
+			else
+			{
+				count[citation]++;
+			}
 		}
-	};
+
+		int total = 0;
+		for (int i = n; i >= 0; i--)
+		{
+			total += count[i];
+			if (total >= i)
+			{
+				return i;
+			}
+		}
+
+		return 1;
+	}
+};
