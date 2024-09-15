6b9b5e52a02be31a076a7b83e200c9e72b68573c
shramim13 <shramim13@gmail.com>
2024-09-15 22:11:13 +0600

    s

---
+++ HashMap/TwoSum.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int x = target - nums[i];
+            if (mp[x] && mp[x] != i)
+            {
+
+                return {i, mp[x]};
+            }
+        }
+        return {};
+    }
+};
