6b5445b885ccf6401b207bd2f4aba29a49b97ae1
shramim13 <shramim13@gmail.com>
2024-09-16 03:51:23 +0600

    s

---
+++ HashMap/LongestConsecutiveSequence.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+        int longest = 0;
+        int n = nums.size();
+        unordered_set<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            st.insert(nums[i]);
+        }
+        for (auto it : st)
+        {
+            if (st.find(it - 1) == st.end())
+            {
+                int cnt = 1;
+                int x = it;
+                while (st.find(x + 1) != st.end())
+                {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = max(longest, cnt);
+            }
+        }
+        return longest;
+    }
+};
