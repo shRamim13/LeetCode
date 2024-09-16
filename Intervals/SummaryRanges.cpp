589e267f6e1ab172b0f541cfcf80e1f8e0056acf
shramim13 <shramim13@gmail.com>
2024-09-16 17:39:37 +0600

    ssd

--- Intervals/SummaryRanges.cpp
+++ Intervals/SummaryRanges.cpp
@@ -1,37 +1,74 @@
+// class Solution
+// {
+// public:
+//     vector<string> summaryRanges(vector<int> &nums)
+//     {
+//         vector<string> v;
+//         if (nums.size() == 0)
+//         {
+//             return v;
+//         }
+//         string start = to_string(nums[0]);
+//         for (int i = 1; i < nums.size(); i++)
+//         {
+//             if (nums[i - 1] + 1 != nums[i])
+//             {
+//                 if (start == to_string(nums[i - 1]))
+//                 {
+//                     v.push_back(to_string(nums[i - 1]));
+//                 }
+//                 else
+//                 {
+//                     v.push_back(start + "->" + to_string(nums[i - 1]));
+//                 }
+//                 start = to_string(nums[i]);
+//             }
+//         }
+//         if (start == to_string(nums.back()))
+//         {
+//             v.push_back(to_string(nums.back()));
+//         }
+//         else
+//         {
+//             v.push_back(start + "->" + to_string(nums.back()));
+//         }
+//         return v;
+//     }
+// };
+
 class Solution
 {
 public:
-    vector<string> summaryRanges(vector<int> &nums)
+    vector<vector<int>> insert(vector<vector<int>> &intervals,
+                               vector<int> &newInterval)
     {
-        vector<string> v;
-        if (nums.size() == 0)
+
+        vector<vector<int>> ans;
+        int i = 0;
+        for (i = 0; i < intervals.size(); i++)
         {
-            return v;
-        }
-        string start = to_string(nums[0]);
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i - 1] + 1 != nums[i])
+            if (newInterval[0] > intervals[i][1])
             {
-                if (start == to_string(nums[i - 1]))
-                {
-                    v.push_back(to_string(nums[i - 1]));
-                }
-                else
-                {
-                    v.push_back(start + "->" + to_string(nums[i - 1]));
-                }
-                start = to_string(nums[i]);
+                ans.push_back(intervals[i]);
+            }
+            else if (newInterval[1] < intervals[i][0])
+            {
+                break;
+            }
+            else
+            {
+                newInterval[0] = min(intervals[i][0], newInterval[0]);
+                newInterval[1] = max(intervals[i][1], newInterval[1]);
             }
         }
-        if (start == to_string(nums.back()))
+
+        ans.push_back(newInterval);
+        while (i < intervals.size())
         {
-            v.push_back(to_string(nums.back()));
+            ans.push_back(intervals[i]);
+            i++;
         }
-        else
-        {
-            v.push_back(start + "->" + to_string(nums.back()));
-        }
-        return v;
+
+        return ans;
     }
 };
