dad6ad268c3309322d40ef2e2a66766a02425672
shramim13 <shramim13@gmail.com>
2024-09-25 13:57:27 +0600

    s

--- Tree/BST/InvertBinaryTree.cpp
+++ Tree/BST/InvertBinaryTree.cpp
@@ -1,3 +1,28 @@
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        // Base case: if the node is null, return null
+        if (root == nullptr)
+        {
+            return root;
+        }
+
+        // Recursively invert the left and right subtrees
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+
+        // Swap the left and right children
+        root->left = right;
+        root->right = left;
+
+        return root;
+    }
+};
+
+// using level order Traversal
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -19,11 +44,22 @@ public:
         {
             return root;
         }
-        TreeNode *temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-        invertTree(root->left);
-        invertTree(root->right);
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            TreeNode *tmp = q.front();
+            q.pop();
+            swap(tmp->left, tmp->right);
+            if (tmp->left != nullptr)
+            {
+                q.push(tmp->left);
+            }
+            if (tmp->right != nullptr)
+            {
+                q.push(tmp->right);
+            }
+        }
         return root;
     }
 };
--- Tree/SameTree.cpp
+++ Tree/SameTree.cpp
@@ -11,3 +11,37 @@ public:
                isSameTree(p->right, q->right);
     }
 };
+
+
+
+// /*
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+//  * right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         if (p == nullptr && q == nullptr) {
+//             return true;
+//         }
+//         if (p == nullptr || q == nullptr) {
+//             return false;
+//         }
+//         if (p->val != q->val) {
+//             return false;
+//         }
+//         bool left = isSameTree(p->left, q->left);
+//         bool right = isSameTree(p->right, q->right);
+//         return left & right;
+//     }
+// };
+// */
