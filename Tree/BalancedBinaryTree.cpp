4cb127d5164daed8a51dfd5c7de4d4dc3683f6f7
shramim13 <shramim13@gmail.com>
2024-09-25 11:07:44 +0600

    b

--- Tree/BalancedBinaryTree.cpp
+++ Tree/BalancedBinaryTree.cpp
@@ -1,34 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution
 {
 public:
-    int bfsHeight(TreeNode *root, int &s)
+    int maxDepth(TreeNode *root)
     {
+
         if (root == nullptr)
         {
             return 0;
         }
-        int lt = bfsHeight(root->left, s);
-        if (lt == -1)
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        if (leftDepth == -1 || rightDepth == -1 ||
+            abs(leftDepth - rightDepth) > 1)
         {
             return -1;
         }
-        int rt = bfsHeight(root->right, s);
-        if (rt == -1)
-        {
-            return -1;
-        }
-        if (abs(lt - rt) > 1)
-        {
-            return -1;
-        }
-        return 1 + max(lt, rt);
+        return 1 + max(leftDepth, rightDepth);
     }
     bool isBalanced(TreeNode *root)
     {
-        int s = 0;
-        s = bfsHeight(root, s);
-        if (s == -1)
+        if (maxDepth(root) == -1)
+        {
             return false;
+        }
         return true;
     }
 };
