948237b8579b9011c4ec138c6b56ca817dc5cec2
shramim13 <shramim13@gmail.com>
2024-09-29 17:04:52 +0600

    s

--- Tree/BST/KthSmallestElementinaBST.cpp
+++ Tree/BST/KthSmallestElementinaBST.cpp
@@ -1,52 +1,48 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+
 class Solution
 {
 public:
-    // void inorder(TreeNode* root, vector<int>& v) {
-    //     if (root == NULL) {
-    //         return;
-    //     }
-    //     inorder(root->left, v);
-    //     v.push_back(root->val);
-    //     inorder(root->right, v);
-    // }
-
-    // int kthSmallest(TreeNode* root, int k) {
-    //     vector<int> v;
-    //     inorder(root, v);
-    //     return v[k-1];
-    // }
-
-    void cntBST(TreeNode *root, int &k, int &ans)
+    int cnt;
+    int result;
+    void solve(TreeNode *root)
     {
-        if (root == nullptr || k == 0)
+        if (root == nullptr)
         {
             return;
         }
-        cntBST(root->left, k, ans);
-        if (--k == 0)
+
+        solve(root->left);
+        cnt--;
+        if (cnt == 0)
         {
-            ans = root->val;
+            result = root->val;
             return;
         }
-        cntBST(root->right, k, ans);
+
+        solve(root->right);
     }
 
     int kthSmallest(TreeNode *root, int k)
     {
-        int ans = 0;
-        cntBST(root, k, ans);
-        return ans;
+        cnt = k;
+        solve(root);
+        return result;
     }
 };
+// class Solution
+// {
+// public:
+//     // void inorder(TreeNode* root, vector<int>& v) {
+//     //     if (root == NULL) {
+//     //         return;
+//     //     }
+//     //     inorder(root->left, v);
+//     //     v.push_back(root->val);
+//     //     inorder(root->right, v);
+//     // }
+
+//     // int kthSmallest(TreeNode* root, int k) {
+//     //     vector<int> v;
+//     //     inorder(root, v);
+//     //     return v[k-1];
+//     // }
