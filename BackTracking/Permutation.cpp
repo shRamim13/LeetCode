276b220de1b80ee031c9b0f305de46e707705364
shramim13 <shramim13@gmail.com>
2024-10-01 18:44:24 +0600

    s

--- BackTracking/Permutation.cpp
+++ BackTracking/Permutation.cpp
@@ -1,26 +1,58 @@
 class Solution
 {
 public:
-    void generatePermutations(vector<int> &nums, int start, vector<vector<int>> &result)
+    vector<vector<int>> ans;
+    void solve(vector<int> &nums, vector<int> &v, vector<int> &vis)
     {
-        if (start == nums.size() - 1)
+        if (v.size() == nums.size())
         {
-            result.push_back(nums);
+            ans.push_back(v);
             return;
         }
-
-        for (int i = start; i < nums.size(); ++i)
+        for (int i = 0; i < nums.size(); i++)
         {
-            swap(nums[start], nums[i]);
-            generatePermutations(nums, start + 1, result);
-            swap(nums[start], nums[i]);
+            if (!vis[i])
+            {
+                vis[i] = 1;
+                v.push_back(nums[i]);
+                solve(nums, v, vis);
+                v.pop_back();
+                vis[i] = 0;
+            }
         }
     }
     vector<vector<int>> permute(vector<int> &nums)
     {
-        vector<vector<int>> permutations;
-        generatePermutations(nums, 0, permutations);
-
-        return permutations;
+        vector<int> v;
+        vector<int> vis(nums.size(), 0);
+        solve(nums, v, vis);
+        return ans;
     }
 };
+
+// class Solution
+// {
+// public:
+//     void generatePermutations(vector<int> &nums, int start, vector<vector<int>> &result)
+//     {
+//         if (start == nums.size() - 1)
+//         {
+//             result.push_back(nums);
+//             return;
+//         }
+
+//         for (int i = start; i < nums.size(); ++i)
+//         {
+//             swap(nums[start], nums[i]);
+//             generatePermutations(nums, start + 1, result);
+//             swap(nums[start], nums[i]);
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int> &nums)
+//     {
+//         vector<vector<int>> permutations;
+//         generatePermutations(nums, 0, permutations);
+
+//         return permutations;
+//     }
+// };
