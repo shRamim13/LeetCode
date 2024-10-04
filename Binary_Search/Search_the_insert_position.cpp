7ab7c3640365b19bb90c3bf6e54327fde1afa88c
shramim13 <shramim13@gmail.com>
2024-10-04 12:09:37 +0600

    s

--- Binary_Search/Search_the_insert_position.cpp
+++ Binary_Search/Search_the_insert_position.cpp
@@ -3,21 +3,24 @@ class Solution
 public:
     int searchInsert(vector<int> &nums, int target)
     {
-        int l = 0, r = nums.size() - 1;
-        int ans = r + 1;
-        while (l <= r)
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
         {
-            int mid = (l + r) / 2;
-            if (nums[mid] >= target)
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
             {
-                ans = mid;
-                r = mid - 1;
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
             }
             else
             {
-                l = mid + 1;
+                right = mid - 1;
             }
         }
-        return ans;
+        return left;
     }
 };
