47266b2fb163ec6a2ded7c2cd636f7e638169298
shramim13 <shramim13@gmail.com>
2024-10-04 15:36:20 +0600

    s

--- Binary_Search/SearchinRotatedSortedArray.cpp
+++ Binary_Search/SearchinRotatedSortedArray.cpp
@@ -1,40 +1,41 @@
-class Solution
+fclass Solution{
+    public :
+        int search(vector<int> & nums, int target){
+            int lt = 0;
+int rt = nums.size() - 1;
+while (lt <= rt)
 {
-public:
-    int search(vector<int> &nums, int target)
+    int mid = lt + (rt - lt) / 2;
+    if (nums[mid] == target)
     {
-        int l = 0, r = nums.size() - 1;
-        int iindx = -1;
-        while (l <= r)
-        {
-            int mid = (l + r) / 2;
-            if (nums[mid] == target)
-            {
-                return mid;
-            }
-            if (nums[l] <= nums[mid])
-            {
-                if (nums[l] <= target && target <= nums[mid])
-                {
-                    r = mid - 1;
-                }
-                else
-                {
-                    l = mid + 1;
-                }
-            }
-            else
-            {
-                if (nums[mid] <= target && target <= nums[r])
-                {
-                    l = mid + 1;
-                }
-                else
-                {
-                    r = mid - 1;
-                }
-            }
-        }
-        return -1;
+        return mid;
     }
-};
+    else if (nums[lt] <= nums[mid])
+    {
+        if (nums[lt] <= target && target < nums[mid])
+        {
+            rt = mid - 1;
+        }
+        else
+        {
+            lt = mid + 1;
+        }
+    }
+    else
+    {
+        if (nums[mid] < target && target <= nums[rt])
+        {
+            lt = mid + 1;
+        }
+        else
+        {
+            rt = mid - 1;
+        }
+    }
+}
+return -1;
+}
+}
+;
+
+// 4 5 6 7 8 9 0 1 2
