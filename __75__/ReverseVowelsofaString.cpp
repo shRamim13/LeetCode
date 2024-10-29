8a76a56cbcc90aacc8819dfe9bc8cc8084cc17dd
shramim13 <shramim13@gmail.com>
2024-10-29 22:35:59 +0600

    s

--- __75__/ReverseVowelsofaString.cpp
+++ __75__/ReverseVowelsofaString.cpp
@@ -108,3 +108,20 @@ public:
 //         return s;
 //     }
 // };
+
+/*
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string token = "";
+
+        string result = "";
+        while (ss >> token) {
+            result = token + " " + result;
+        }
+        return result.substr(0, result.length() - 1);
+    }
+};
+*/
