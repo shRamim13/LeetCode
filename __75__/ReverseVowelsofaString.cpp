2d069cbf932858148f0d2c25777dd9acce9b2b43
shramim13 <shramim13@gmail.com>
2024-10-29 21:03:25 +0600

    a

--- __75__/ReverseVowelsofaString.cpp
+++ __75__/ReverseVowelsofaString.cpp
@@ -38,3 +38,73 @@ public:
         return s;
     }
 };
+
+/*
+
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'A' || c == 'e' || c == 'E' ||
+               c == 'i' || c == 'I' || c == 'o' || c == 'O' ||
+               c == 'u' || c == 'U';
+    }
+
+    string reverseVowels(string s) {
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            // Move the left pointer to the next vowel
+            while (left < right && !isVowel(s[left])) {
+                left++;
+            }
+
+            // Move the right pointer to the previous vowel
+            while (left < right && !isVowel(s[right])) {
+                right--;
+            }
+
+            // Swap the vowels
+            if (left < right) {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
+
+
+*/
+
+// #include <unordered_set>
+// #include <string>
+// using namespace std;
+
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+//         int left = 0, right = s.length() - 1;
+
+//         while (left < right) {
+//             // Move left pointer to the next vowel
+//             while (left < right && vowels.find(s[left]) == vowels.end()) {
+//                 left++;
+//             }
+//             // Move right pointer to the previous vowel
+//             while (left < right && vowels.find(s[right]) == vowels.end()) {
+//                 right--;
+//             }
+//             // Swap the vowels
+//             if (left < right) {
+//                 swap(s[left], s[right]);
+//                 left++;
+//                 right--;
+//             }
+//         }
+//         return s;
+//     }
+// };
