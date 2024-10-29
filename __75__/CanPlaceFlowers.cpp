ff6ad3cf575c879b93d6d46de0a1ef42dec07fca
shramim13 <shramim13@gmail.com>
2024-10-29 20:19:04 +0600

    s

---
+++ __75__/CanPlaceFlowers.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.size(); i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                int lt = (i == 0 || flowerbed[i - 1] == 0);
+                int rt = (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0);
+                if (lt && rt)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                    i++;
+                }
+            }
+        }
+        return n <= 0;
+    }
+};
