80e4d3d69e80f5e4d9ae02ef856870e57a2f4a34
shramim13 <shramim13@gmail.com>
2024-12-01 04:31:18 +0600

    s

--- __75__/ReverseVowelsofaString.cpp
+++ __75__/ReverseVowelsofaString.cpp
@@ -39,73 +39,38 @@ public:
     }
 };
 
-/*
-
-class Solution {
+class Solution
+{
 public:
-    bool isVowel(char c) {
-        return c == 'a' || c == 'A' || c == 'e' || c == 'E' ||
-               c == 'i' || c == 'I' || c == 'o' || c == 'O' ||
-               c == 'u' || c == 'U';
-    }
-
-    string reverseVowels(string s) {
-        int n = s.length();
-        int left = 0;
-        int right = n - 1;
-
-        while (left < right) {
-            // Move the left pointer to the next vowel
-            while (left < right && !isVowel(s[left])) {
-                left++;
+    string reverseVowels(string s)
+    {
+        stack<char> stk;
+        unordered_set<char> st;
+        st.insert('a');
+        st.insert('e');
+        st.insert('i');
+        st.insert('o');
+        st.insert('u');
+        st.insert('A');
+        st.insert('E');
+        st.insert('I');
+        st.insert('O');
+        st.insert('U');
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (st.find(s[i]) != st.end())
+            {
+                stk.push(s[i]);
             }
-
-            // Move the right pointer to the previous vowel
-            while (left < right && !isVowel(s[right])) {
-                right--;
-            }
-
-            // Swap the vowels
-            if (left < right) {
-                swap(s[left], s[right]);
-                left++;
-                right--;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (st.find(s[i]) != st.end())
+            {
+                s[i] = stk.top();
+                stk.pop();
             }
         }
         return s;
     }
 };
-
-
-*/
-
-// #include <unordered_set>
-// #include <string>
-// using namespace std;
-
-// class Solution {
-// public:
-//     string reverseVowels(string s) {
-//         unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-//         int left = 0, right = s.length() - 1;
-
-//         while (left < right) {
-//             // Move left pointer to the next vowel
-//             while (left < right && vowels.find(s[left]) == vowels.end()) {
-//                 left++;
-//             }
-//             // Move right pointer to the previous vowel
-//             while (left < right && vowels.find(s[right]) == vowels.end()) {
-//                 right--;
-//             }
-//             // Swap the vowels
-//             if (left < right) {
-//                 swap(s[left], s[right]);
-//                 left++;
-//                 right--;
-//             }
-//         }
-//         return s;
-//     }
-// };
-
